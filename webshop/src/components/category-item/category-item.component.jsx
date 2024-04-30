c781a2dfb0f202d0c4fc99cb5ee31299c5cc89ed
Matyas <mathias.hun@gmail.com>
2024-04-30 11:47:29 +0200

    Updated the App with menu component and item component

---
+++ webshop/src/components/category-item/category-item.component.jsx
@@ -0,0 +1,14 @@
+import './category-item.component.scss'
+
+export default function CategoryItem({category}) {
+  const {imageUrl, title} = category
+  return(
+    <div className='category-container'>
+          <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </div> 
+        </div> 
+  )
+}
