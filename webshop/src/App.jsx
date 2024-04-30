f6495ca07ec18435b1bcbde26c52e9b4a4f554ec
Matyas <mathias.hun@gmail.com>
2024-04-30 09:41:34 +0200

    created the base

---
+++ webshop/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    },
+  ]
+
+  return (
+    <div className='categories-container'>
+        {categories.map(({ title }) => (
+                <div className='category-container'>
+                {/* <img src="" alt="" /> */}
+              <div className='category-body-container'>
+                <h2>{title}</h2>
+                <p>Shop now</p>
+              </div> 
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default App
